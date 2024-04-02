ea8d8c3a64f0a8fcb2f7ca1c34d9dcf875818e2a
arnaubena97 <arnaubenavides97@gmail.com>
2024-04-02 12:17:03 +0200

    Add models

---
+++ Data/Entities/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace collectionsapi.Data.Entities
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public User? User { get; set; }
+    }
+}
---
+++ Data/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace collectionsapi.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        [JsonIgnore]
+        public List<Collection>? Collections { get; set; }
+    }
+
+}
