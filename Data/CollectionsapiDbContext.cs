43d97bb9f09c07200508523a25021573c765c679
arnaubena97 <arnaubenavides97@gmail.com>
2024-04-02 13:01:08 +0200

    add bbdd

---
+++ Data/CollectionsapiDbContext.cs
@@ -0,0 +1,30 @@
+﻿using collectionsapi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace collectionsapi.Data
+{
+    public class CollectionsapiDbContext : DbContext
+    {
+        public CollectionsapiDbContext(DbContextOptions<CollectionsapiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                        .HasMany(u => u.Collections)
+                        .WithOne(c => c.User)
+                        .HasForeignKey(c => c.UserId)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+        }
+
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Load configuration
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+
+// Configure the database context
+builder.Services.AddDbContext<collectionsapi.Data.CollectionsapiDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
